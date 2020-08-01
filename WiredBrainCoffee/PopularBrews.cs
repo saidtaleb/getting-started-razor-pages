490162056346cbfb647b2e436004a238976fc036
Said Taleb <said@inovasquad.com>
2020-08-02 00:01:12 +0100

    working with views

---
+++ WiredBrainCoffee/PopularBrews.cs
@@ -0,0 +1,17 @@
+﻿namespace WiredBrainCoffee
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using WiredBrainCoffee.Services;
+
+    public class PopularBrews : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int count)
+        {
+            var menu = new MenuService();
+            var items = menu.GetMenuItems().Take(count);
+            return View(items);
+        }
+    }
+}
